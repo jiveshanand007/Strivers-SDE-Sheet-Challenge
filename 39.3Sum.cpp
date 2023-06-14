8db3d29ea81c33c488364134138a0e00468d3055
jiveshanand007 <jiveshanand007@gmail.com>
2023-06-15 02:45:48 +0530

    Create 39.3Sum.cpp

---
+++ 39.3Sum.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+vector<vector<int>> findTriplets(vector<int> arr, int n, int K)
+{
+    vector<vector<int>> ans;
+    sort(arr.begin(), arr.end());
+    for (int i = 0; i < n - 2; i++)
+    {
+        if (i == 0 || i > 0 && arr[i] != arr[i - 1])
+        {
+            int low = i + 1, high = n - 1, sum = K - arr[i];
+            while (low < high)
+            {
+                if (arr[low] + arr[high] == sum)
+                {
+                    vector<int> temp;
+                    temp.push_back(arr[i]);
+                    temp.push_back(arr[low]);
+                    temp.push_back(arr[high]);
+                    ans.push_back(temp);
+                    while (low < high && arr[low] == arr[low + 1])
+                        low++;
+                    while (low < high && arr[high] == arr[high - 1])
+                        high--;
+                    low++;
+                    high--;
+                }
+                else if (arr[low] + arr[high] < sum)
+                    low++;
+                else
+                    high--;
+            }
+        }
+    }
+
+    return ans;
+}
