d284fee8a4aa8402e08fcc89e9ff706d191ab57e
jiveshanand007 <jiveshanand007@gmail.com>
2023-07-04 22:37:43 +0530

    Create 110.Compare Version Numbers.cpp

---
+++ 110.Compare Version Numbers.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+int compareVersions(string a, string b)
+{
+    int n = a.length();
+    int m = b.length();
+    long long num1 = 0, num2 = 0;
+    int i = 0, j = 0;
+    while (i < n || j < m)
+    {
+        while (i < n && a[i] != '.')
+        {
+            num1 = num1 * 10 + (a[i] - '0');
+            i++;
+        }
+        while (j < m && b[j] != '.')
+        {
+            num2 = num2 * 10 + (b[j] - '0');
+            j++;
+        }
+        if (num1 > num2)
+            return 1;
+        if (num1 < num2)
+            return -1;
+        i++;
+        j++;
+        num1 = 0, num2 = 0;
+    }
+    return 0;
+}
