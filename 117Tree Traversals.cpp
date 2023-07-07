088e71ac1f5f9d7b9a2a7bd8ae8e735ab0b36333
jiveshanand007 <jiveshanand007@gmail.com>
2023-07-07 22:33:04 +0530

    Create 117Tree Traversals.cpp

---
+++ 117Tree Traversals.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+/************************************************************
+
+    Following is the Binary Tree node structure:
+
+    class BinaryTreeNode
+    {
+    public :
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+
+************************************************************/
+
+vector<int> inorder;
+
+vector<int> preorder;
+
+vector<int> postorder;
+
+void solve(BinaryTreeNode<int> *root)
+
+{
+
+    if (!root)
+        return;
+
+    preorder.push_back(root->data);
+
+    solve(root->left);
+
+    inorder.push_back(root->data);
+
+    solve(root->right);
+
+    postorder.push_back(root->data);
+}
+
+vector<vector<int>> getTreeTraversal(BinaryTreeNode<int> *root)
+{
+
+    // Write your code here.
+
+    vector<vector<int>> ans;
+
+    solve(root);
+
+    ans.push_back(inorder);
+
+    ans.push_back(preorder);
+
+    ans.push_back(postorder);
+
+    inorder.clear();
+
+    preorder.clear();
+
+    postorder.clear();
+
+    return ans;
+}
