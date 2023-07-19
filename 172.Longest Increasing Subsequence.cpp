163799613270361fb8e271ef38147bbd8d7d4af6
jiveshanand007 <jiveshanand007@gmail.com>
2023-07-19 11:23:54 +0530

    Create 172.Longest Increasing Subsequence.cpp

---
+++ 172.Longest Increasing Subsequence.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+int longestIncreasingSubsequence(int arr[], int n)
+{
+    vector<int> temp;
+    temp.push_back(arr[0]);
+
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] > temp.back())
+        {
+            temp.push_back(arr[i]);
+        }
+        else
+        {
+            int idx = lower_bound(temp.begin(), temp.end(), arr[i]) - temp.begin();
+            temp[idx] = arr[i];
+        }
+    }
+    return temp.size();
+}
