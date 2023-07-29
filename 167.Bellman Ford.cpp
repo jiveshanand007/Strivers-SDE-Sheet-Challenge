13dc2799ab8977a64bc2c5643ca852798059d16b
jiveshanand007 <jiveshanand007@gmail.com>
2023-07-29 16:36:24 +0530

    Create 167.Bellman Ford.cpp

---
+++ 167.Bellman Ford.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+int bellmonFord(int n, int m, int src, int dest, vector<vector<int>> &edges)
+{
+    vector<int> dist(n + 1, 1e9);
+    dist[src] = 0;
+    // relaxation done till n-1 times if we do 1 more relaxation and dist gets reduced means graph contains negative cycle
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (auto it : edges)
+        {
+            int u = it[0];
+            int v = it[1];
+            int wt = it[2];
+            if (dist[u] != 1e9 && dist[u] + wt < dist[v])
+            {
+                dist[v] = dist[u] + wt;
+            }
+        }
+    }
+    return dist[dest];
+}
